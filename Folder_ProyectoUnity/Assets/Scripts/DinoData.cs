6405781bd375d035278a9cf060247c732f58f0c5
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2024-10-27 14:01:23 -0500

    Avance del escenario - faltan correcciones

--- Folder_ProyectoUnity/Assets/Scripts/DinoData.cs
+++ Folder_ProyectoUnity/Assets/Scripts/DinoData.cs
@@ -6,12 +6,8 @@ using TMPro;
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/DinoData", order = 1)]
 public class DinoData : ScriptableObject
 {
-    [SerializeField] private GameObject dinosaur;
     [SerializeField] private TextMeshProUGUI nameDino;
-    [SerializeField] private TextMeshProUGUI lifeDinoData;
     [SerializeField] private TextMeshProUGUI damageDinoData;
-    [SerializeField] private int lifeDino;
     [SerializeField] private int damageDino;
-
 
 }
--- Folder_ProyectoUnity/Assets/Scripts/PlayerController.cs
+++ Folder_ProyectoUnity/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,6 @@ public class PlayerControler : MonoBehaviour
     private Rigidbody rb;
     private Vector2 moveInput;
     public float speed = 5f;
-    public float jumpForce = 7f;
-    private bool isGrounded;
 
     private void Awake()
     {
@@ -17,34 +15,9 @@ public class PlayerControler : MonoBehaviour
     {
         moveInput = context.ReadValue<Vector2>();
     }
-
-    public void OnJump(InputAction.CallbackContext context)
-    {
-        if (isGrounded && context.performed)
-        {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        }
-    }
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(moveInput.x, 0, moveInput.y) * speed * Time.deltaTime;
         rb.MovePosition(transform.position + movement);
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true;
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = false;
-        }
-
     }
 }
