801fbf3f8d5304e94ee695379c77b70e9a3eae37
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2024-11-05 13:01:39 -0500

    Suavizado Camara

--- Folder_ProyectoUnity/Assets/Scripts/PlayerController.cs
+++ Folder_ProyectoUnity/Assets/Scripts/PlayerController.cs
@@ -3,8 +3,13 @@ using UnityEngine.InputSystem;
 public class PlayerControler : MonoBehaviour
 {
     private Rigidbody rb;
+
     private Vector2 moveInput;
     [SerializeField]private float speed;
+
+    [Header("Rotacion y velocidad")]
+    [SerializeField] float velocidadRotacion;
+    [SerializeField] float giro;
 
     private void Awake()
     {
@@ -17,7 +22,18 @@ public class PlayerControler : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        Vector3 movement = new Vector3(moveInput.x, 0, moveInput.y) * speed * Time.deltaTime;
-        rb.MovePosition(transform.position + movement);
+        Vector3 direction = new Vector3(moveInput.x, 0, moveInput.y).normalized;
+
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref velocidadRotacion, giro);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            rb.MovePosition(transform.position + moveDirection * speed * Time.deltaTime);
+
+        }
     }
 }
